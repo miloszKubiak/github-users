a292a4dfcd84e5392d339f64687082062e6a9e61
miloszKubiak <milypan69@gmail.com>
2022-07-15 15:19:24 +0200

    create error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>sorry, page not found :(</h3>
+        <Link to="/" className='btn'>
+          back home
+        </Link>
+      </div>
+    </Wrapper>
   )
 }
 
-export default Error
+export default Error;
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  text-align: center;
+  background: var(--primary-10);
+
+  h1 {
+    font-size: 10rem;
+  }
+
+  h3 {
+    margin-bottom: 1.5rem;
+  }
+`;
