bd7951748b91f4f4f69971363249e9c968f9d408
miloszKubiak <milypan69@gmail.com>
2022-07-21 10:23:05 +0200

    add a function that checks the requests limit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import styled from "styled-components";
 import { MdSearch } from "react-icons/md";
+import { GithubContext } from "../context/context";
 
 const Search = () => {
 	const [user, setUser] = useState("");
+	const { requests } = useContext(GithubContext);
+	console.log(requests);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -26,10 +29,10 @@ const Search = () => {
 							value={user}
 							onChange={(e) => setUser(e.target.value)}
 						/>
-						<button type="submit">search</button>
+						{requests > 0 && <button type="submit">search</button>}
 					</div>
 				</form>
-				<h3>requests: 60/60</h3>
+				<h3>requests: {requests}/60</h3>
 			</Wrapper>
 		</section>
 	);
@@ -45,71 +48,74 @@ const Wrapper = styled.div`
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
-	
+
+	@media (max-width: 550px) {
+		flex-direction: column;
+	}
+
 	h3 {
-		padding: 0 .5rem;
+		padding: 0 0.5rem;
 	}
 
 	.form-control {
-    background: var(--white);
-    display: grid;
-    align-items: center;
-    grid-template-columns: auto 1fr auto;
-    column-gap: 0.5rem;
-    border-radius: 5px;
-    padding: 0.5rem;
+		background: var(--white);
+		display: flex;
+		align-items: center;
+		border-radius: 5px;
+		padding: 0.5rem;
+		margin: 0.5rem;
 
-    input {
-      border-color: transparent;
-      outline-color: var(--grey-10);
-      letter-spacing: var(--spacing);
-      color: var(--grey-3);
-      padding: 0.2rem 0.5rem;
-    }
+		input {
+			border-color: transparent;
+			outline-color: var(--grey-10);
+			letter-spacing: var(--spacing);
+			color: var(--grey-3);
+			padding: 0.2rem 0.5rem;
+			margin: 0 0.5rem;
+		}
 
-    input::placeholder {
-      color: var(--grey-3);
-      text-transform: capitalize;
-      letter-spacing: var(--spacing);
-    }
+		input::placeholder {
+			color: var(--grey-3);
+			text-transform: capitalize;
+			letter-spacing: var(--spacing);
+		}
 
-    button {
-      border-radius: 5px;
-      border-color: transparent;
-      padding: 0.2rem 0.5rem;
-      text-transform: capitalize;
-      letter-spacing: var(--spacing);
-      background: var(--primary-5);
-      color: var(--white);
-      transition: var(--transition);
-      cursor: pointer;
-      &:hover {
-        background: var(--primary-8);
-        color: var(--primary-1);
-      }
-    }
+		button {
+			border-radius: 5px;
+			border-color: transparent;
+			padding: 0.2rem 0.5rem;
+			text-transform: capitalize;
+			letter-spacing: var(--spacing);
+			background: var(--primary-5);
+			color: var(--white);
+			transition: var(--transition);
+			cursor: pointer;
+			&:hover {
+				background: var(--primary-8);
+				color: var(--primary-1);
+			}
+		}
 
-    svg {
-      color: var(--grey-5);
-    }
+		svg {
+			color: var(--grey-5);
+		}
 
-    input,
-    button,
-    svg {
-      font-size: 1.3rem;
-    }
-    @media (max-width: 800px) {
-      button,
-      input,
-      svg {
-        font-size: 0.8rem;
-      }
-    }
-  }
-  h3 {
-    margin-bottom: 0;
-    color: var(--grey-5);
-    font-weight: 400;
-  }
+		input,
+		button,
+		svg {
+			font-size: 1.3rem;
+		}
+		@media (max-width: 800px) {
+			button,
+			input,
+			svg {
+				font-size: 0.8rem;
+			}
+		}
+	}
+	h3 {
+		margin-bottom: 0;
+		color: var(--grey-5);
+		font-weight: 400;
+	}
 `;
-
--- src/context/context.js
+++ src/context/context.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import mockUser from "./mockData/mockUser";
 import mockFollowers from "./mockData/mockFollowers";
 import mockRepos from "./mockData/mockRepos";
+import axios from "axios";
 
 const ROOT_URL = "https://api.github.com";
 
@@ -12,8 +13,32 @@ const GithubProvider = ({ children }) => {
 	const [followers, setFollowers] = useState(mockFollowers);
 	const [repos, setRepos] = useState(mockRepos);
 
+	// request loading
+	const [requests, setRequests] = useState(0);
+	const [loading, setLoading] = useState(false);
+
+	//check rate
+	const checkRequests = () => {
+		axios(`${ROOT_URL}/rate_limit`)
+			.then(({ data }) => {
+				let {
+					rate: { remaining },
+				} = data;
+				setRequests(remaining);
+				if (remaining === 0) {
+					//throw error
+				}
+			})
+			.catch((error) => console.log(error));
+	};
+
+	//error
+	useEffect(checkRequests, []);
+
 	return (
-		<GithubContext.Provider value={{ githubUser, followers, repos }}>
+		<GithubContext.Provider
+			value={{ githubUser, followers, repos, requests }}
+		>
 			{children}
 		</GithubContext.Provider>
 	);
